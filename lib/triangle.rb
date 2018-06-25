22395e1206ad37243b830e6ecd21e552c6981030
Mark Lawrence <lawrence.ml@gmail.com>
2018-06-25 03:29:07 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,10 @@ class Triangle
   def kind
     sorted = [a, b, c].sort
     if a + b <= c || a <= 0 || b || 0 || c <= 0
-      raise TriangleError
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+      end
     elsif a == b && b == c
       :equilateral
     elsif a == c || b == c || a ==b
