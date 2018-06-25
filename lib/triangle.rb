308f2036e1a41ca5e23e1cde8b41fc8754f45449
Mark Lawrence <lawrence.ml@gmail.com>
2018-06-25 03:22:43 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,7 +15,7 @@ class Triangle
     #   :isosceles
     # elsif a > 0 && b > 0 && c > 0
     #   :scalene
-    if a + b <= c
+    if a + b <= c || sorted > 0
       raise TriangleError
     end
   end
