6581d103dbfa5b03647a9740abc8f2ab3e3ad0af
Mark Lawrence <lawrence.ml@gmail.com>
2018-06-25 03:05:20 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,18 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    sorted = [a, b, c].sort
+    if a == b && b == c
+      :equilateral
+    elsif a == c && b > 0
+      :isosceles
+    end
+  end
 end
